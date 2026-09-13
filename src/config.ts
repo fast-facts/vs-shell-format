a0c67b1446403df0a029dbcf25f2607d23cd3954
fast-facts <107660271+fast-facts@users.noreply.github.com>
2026-09-13 18:38:33 -0400

    feat: update shfmt to v3.14.1 (#31)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  shfmtVersion: 'v3.13.1',
+  shfmtVersion: 'v3.14.1',
   needCheckInstall: true,
   shfmtChecksums: {
-    'shfmt_v3.13.1_darwin_amd64': '6feedafc72915794163114f512348e2437d080d0047ef8b8fa2ec63b575f12af',
-    'shfmt_v3.13.1_darwin_arm64': '9680526be4a66ea1ffe988ed08af58e1400fe1e4f4aef5bd88b20bb9b3da33f8',
-    'shfmt_v3.13.1_linux_386': '5f9a2c46249dab66d7767df676083ea6a3313831ca9f4bc29e4c21af87bd4f5e',
-    'shfmt_v3.13.1_linux_amd64': 'fb096c5d1ac6beabbdbaa2874d025badb03ee07929f0c9ff67563ce8c75398b1',
-    'shfmt_v3.13.1_linux_arm': 'f6c23b95971bcc5ebf86411bccefb79f1e99f8efe7c722d980a3f244838b1398',
-    'shfmt_v3.13.1_linux_arm64': '32d92acaa5cd8abb29fc49dac123dc412442d5713967819d8af2c29f1b3857c7',
-    'shfmt_v3.13.1_windows_386.exe': '9b4f368b837feb883a3b2bf38a46a94dcab4bca4b7c3e19f6710e0e09a56ce7c',
-    'shfmt_v3.13.1_windows_amd64.exe': '60cd368533d0ad73fa86d93d5bbf95ef40587245ce684ed138c1b31557b5fe97',
+    'shfmt_v3.14.1_darwin_amd64': 'd33eee0da0f92835b3562e9767a05cee7e4eaeef47daa03bfd09da17b4b590a6',
+    'shfmt_v3.14.1_darwin_arm64': 'b7c872db63553ccffc7253aba3ed7d4885a27d83f1ba567b1138c6315a5847e5',
+    'shfmt_v3.14.1_linux_386': 'aad6f0bd6285910c152091ba58b5509aa88eb8a2a81a2d72cf2048a8a78c27f4',
+    'shfmt_v3.14.1_linux_amd64': '76e77641faa025814b77f153b29796b8e6fa2fca03e0c76a691608b86c7ea7bf',
+    'shfmt_v3.14.1_linux_arm': 'ede6c4adefeda196d963906daa7c1f20094e6def7a98bcb9f4c74079babf1eca',
+    'shfmt_v3.14.1_linux_arm64': '5f2db09dae91fca848f7adbdd014632e921a383863a2ad7e0450ad3aba0c6489',
+    'shfmt_v3.14.1_windows_386.exe': 'b6f171113e7359f44d5ffdc93a90b93a7d405aa8847129a6982e10fe4d356f1e',
+    'shfmt_v3.14.1_windows_amd64.exe': '13629ce28442ca80b6b5a819f7574ab39e1c28c6e26734ca816c9714e04851df',
   } as const,
 };
