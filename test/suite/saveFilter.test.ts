1d6207a41de2fa6784f2c8156618cc98cf404c8b
fast-facts <107660271+fast-facts@users.noreply.github.com>
2026-09-13 16:25:50 -0400

    test: lock isAllowedTextDocument language and scheme filter (#19)

---
+++ test/suite/saveFilter.test.ts
@@ -0,0 +1,21 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+import { isAllowedTextDocument } from '../../src/extension';
+
+suite('isAllowedTextDocument', () => {
+  test('allows a listed language on untitled', async () => {
+    const document = await vscode.workspace.openTextDocument({
+      language: 'shellscript',
+      content: 'echo hi\n',
+    });
+    assert.strictEqual(isAllowedTextDocument(document), true);
+  });
+
+  test('rejects a language that is not in the list', async () => {
+    const document = await vscode.workspace.openTextDocument({
+      language: 'plaintext',
+      content: 'not a shell file\n',
+    });
+    assert.strictEqual(isAllowedTextDocument(document), false);
+  });
+});
