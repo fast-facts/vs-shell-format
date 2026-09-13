8534a303ee828ba1c854b20f1a5dac072bc41f7d
fast-facts <107660271+fast-facts@users.noreply.github.com>
2026-09-13 16:33:18 -0400

    chore: remove unused PATH lookup
    Nothing live called getExecutableFileUnderPath after the unused install and diff helpers were removed.

--- src/pathUtil.ts
+++ src/pathUtil.ts
@@ -1,35 +1,5 @@
 import * as fs from 'fs';
-import * as path from 'path';
 import * as vscode from 'vscode';
-
-let binPathCache: { [bin: string]: string } = {};
-export function getExecutableFileUnderPath(toolName: string) {
-  let cachePath = binPathCache[toolName];
-  if (cachePath) {
-    return cachePath;
-  }
-  toolName = correctBinname(toolName);
-  if (path.isAbsolute(toolName)) {
-    return toolName;
-  }
-  let paths = process.env['PATH'].split(path.delimiter);
-  for (let i = 0; i < paths.length; i++) {
-    let binpath = path.join(paths[i], toolName);
-    if (fileExists(binpath)) {
-      binPathCache[toolName] = binpath;
-      return binpath;
-    }
-  }
-  return null;
-}
-
-function correctBinname(binname: string) {
-  if (process.platform === 'win32' && path.extname(binname) !== '.exe') {
-    return binname + '.exe';
-  } else {
-    return binname;
-  }
-}
 
 export function fileExists(filePath: string): boolean {
   try {
